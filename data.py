133974060fbbbaf088d5318cad900c39b47422f2
Leni Shor <leni@mit.edu>
2024-07-17 14:19:18 -0400

    implement vanilla indexing task

--- data.py
+++ data.py
@@ -5,58 +5,53 @@ from typing import Iterator, Optional
 from torch.utils.data import IterableDataset
 
 
-class SelectApplyDataset(IterableDataset):
-    """An iterable dataset for a select-apply task.
-
-    The input consists of an array `xs` and an index `i`.
-    The output is the value `y` obtained by applying a fixed map `f` to the selected element `x = xs[i]` of the array `xs`.
-    The task, therefore, requires the composition of two distinct capabilities:
-    the *selection* of the array element corresponding to the given index and the *application* of a function to the selected element.
-
-    Currently, only supports the special case in which learning a capability in isolation does not lead to a reduction in loss.
-    """
+class IndexingDataset(IterableDataset):
+    """A dataset for an indexing task. Samples are drawn uniformly."""
 
     def __init__(
         self,
-        domain_size: int,
-        range_size: int,
+        num_symbols: int,
         array_len: int,
+        batch_size: int = 1,
         device: torch.device = torch.device("cpu"),
         seed: Optional[int] = None,
     ) -> None:
-        if array_len % range_size != 0:
-            raise ValueError(f"range size {range_size} must divide array length {array_len}")
+        self.num_symbols = num_symbols
+        self.array_len = array_len
 
-        if domain_size % range_size != 0:
-            raise ValueError(f"range size {range_size} must divide domain size {domain_size}")
+        self.batch_size = batch_size
+        self.device = device
+        self.seed = seed
 
-        self.domain_size: int = domain_size
-        self.range_size: int = range_size
-        self.kernel_size: int = domain_size // range_size
-        self.array_len: int = array_len
-        self.seq_len: int = array_len + 1
-
-        self.device: torch.device = device
-        self.seed: Optional[int] = seed
-
-        self.generator: torch.Generator = torch.Generator(device=device)
+        self.gen = torch.Generator(device=device)
         if self.seed is not None:
-            self.generator.manual_seed(seed)
+            self.gen.manual_seed(seed)
 
     def __iter__(self) -> Iterator:
         return self
 
-    def __next__(self) -> tuple[torch.Tensor, torch.Tensor]:
-        permutation = torch.randperm(n=self.array_len, generator=self.generator, device=self.device)
-        ys = permutation % self.range_size
-        xs = ys + self.range_size * torch.randint(
+    def __next__(self) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Yields an instance of the indexing task.
+
+        Shape:
+            - array: (batch_size, array_len)
+            - index: (batch_size,)
+            - target: (batch_size,)
+        """
+        array = torch.randint(
             low=0,
-            high=self.kernel_size,
-            size=(self.array_len,),
-            generator=self.generator,
+            high=self.num_symbols,
+            size=(self.batch_size, self.array_len),
             device=self.device,
+            generator=self.gen,
         )
-        i = torch.randint(low=0, high=self.array_len, size=(1,), generator=self.generator, device=self.device)
-        y = ys[i].squeeze()
-        seq = torch.concatenate([xs, i])
-        return seq, y
+        index = torch.randint(
+            low=0,
+            high=self.array_len,
+            size=(self.batch_size,),
+            device=self.device,
+            generator=self.gen,
+        )
+        target = array[torch.arange(self.batch_size), index]
+        return array, index, target
