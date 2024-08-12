88be6435ba5cddda63f9376eb6a4696003bf675d
Leni Shor <leni@mit.edu>
2024-08-12 19:42:51 -0400

    implement non-fused toy transformer

--- models.py
+++ models.py
@@ -1,75 +1,50 @@
 import torch
 import torch.nn as nn
+
 from math import sqrt
-from config import RunConfig
 
 
 class ToyTransformer(nn.Module):
     """A simplified transformer model for indexing tasks."""
 
-    def __init__(self, num_symbols: int, max_array_len: int, embedding_dim: int) -> None:
+    def __init__(self, vocab_size: int, sequence_len: int, embedding_dim: int) -> None:
         """Initializes the toy transformer."""
         super().__init__()
 
-        self.num_symbols = num_symbols
-        self.max_array_len = max_array_len
+        self.vocab_size = vocab_size
+        self.sequence_len = sequence_len
         self.embedding_dim = embedding_dim
 
-        self.query_map = nn.Embedding(max_array_len, embedding_dim)
-        self.key_map = nn.Embedding(max_array_len, embedding_dim)
-        self.value_map = nn.Embedding(num_symbols, num_symbols)
+        # TODO: figure out if there's a nicer way of initializing these that's as convenient for downstream use
+        self.query_map = nn.Embedding(sequence_len, embedding_dim)
+        self.keys = nn.Parameter(torch.randn(sequence_len, embedding_dim))
+        self.value_map = nn.Embedding(vocab_size, embedding_dim)
+        self.output_matrix = nn.Linear(embedding_dim, vocab_size, bias=False)
 
-        # normalize weights
-        with torch.no_grad():
-            self.query_map.weight.div_(sqrt(embedding_dim))
-            self.key_map.weight.div_(sqrt(embedding_dim))
-            self.value_map.weight.div_(sqrt(num_symbols))
-
-    def forward(self, array: torch.Tensor, index: torch.Tensor) -> torch.Tensor:
-        """Performs a forward pass through the toy transformer.
+    def forward(self, sequence: torch.Tensor, index: torch.Tensor) -> torch.Tensor:
+        """Does a forward pass through the toy transformer.
 
         Shape:
-            - array: (batch_size, array_len)
+            - sequence: (batch_size, sequence_len)
             - index: (batch_size,)
             - output: (batch_size, num_symbols)
         """
         # compute query
         query = self.query_map(index)
 
-        # compute keys
-        positions = torch.arange(self.max_array_len, device=array.device)
-        keys = self.key_map(positions)
-
-        # compute values
-        values = self.value_map(array)
+        # compute preattention
+        preattention = (query @ self.keys) / sqrt(self.embedding_dim)
 
         # compute attention
-        preattention = torch.einsum("bd, jd -> bj", query, keys)
         attention = preattention.softmax(dim=-1)
-        return torch.einsum("bj, bjv -> bv", attention, values)
 
-    def qk_circuit(self) -> torch.Tensor:
-        """Returns a matrix of query-key dot products.
+        # compute values
+        values = self.value_map(sequence)
 
-        Shape:
-            - output: (max_array_len, max_array_len)
-        """
-        queries = self.query_map.weight
-        keys = self.key_map.weight
-        return torch.einsum("qd, kd -> qk", queries, keys)
+        # compute results
+        results = (attention @ values).sum(dim=-2)
 
-    def value_circuit(self) -> torch.Tensor:
-        """Returns a matrix of symbol logits.
+        # compute output
+        output = self.output_matrix(results)
 
-        Shape:
-            - output: (num_symbols, num_symbols)
-        """
-        return self.value_map.weight
-
-
-def get_model(config: RunConfig) -> ToyTransformer:
-    return ToyTransformer(
-        num_symbols=config.data.num_symbols,
-        max_array_len=config.data.array_len,
-        embedding_dim=config.model.embedding_dim,
-    )
+        return output
